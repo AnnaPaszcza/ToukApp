7f68d1b1e2c97f24c56845a4a79eefbee1f23ad3
annap <anna.paszcza@gmail.com>
2022-02-07 10:35:51 +0100

    Pull request

--- curlToukApp.sh
+++ curlToukApp.sh
@@ -1,3 +1,3 @@
 curl -X GET http://localhost:8080/api/screening/"2022-03-01"/"10:00:00"
 curl -X GET http://localhost:8080/api/screening/1
-curl -X POST -H "Content-type: application/json" -d "{\"name\": \"John\", \"surname\": \"Smith\", \"screeningId\": 1, \"chosenSeatIds\": [{ \"key\": \"16\", \"value\": \"1\"}, { \"key\": \"16\", \"value\": \"1\"}, { \"key\": \"16\", \"value\": \"1\"}]}" http://localhost:8080/api/makeReservation
+curl -X POST http://localhost:8080/api/makeReservation -H "Content-type: application/json" -d "{\"name\": \"John\", \"surname\": \"Smith\", \"screeningId\": 1, \"chosenSeatIds\": [{ \"key\": \"16\", \"value\": \"1\"}, { \"key\": \"16\", \"value\": \"1\"}, { \"key\": \"16\", \"value\": \"1\"}]}"
